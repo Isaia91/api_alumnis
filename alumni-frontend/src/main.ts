3555b39af683340b0b81eed745c95e5292e8433d
Isaïa Maperi <isaia.maperi@gmail.com>
2025-10-27 19:55:53 +1100

    netlify update

--- alumni-frontend/src/main.ts
+++ alumni-frontend/src/main.ts
@@ -1,13 +1,11 @@
-// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
-    provideRouter(routes),
-  ],
-});
+    provideRouter(routes, withHashLocation())
+  ]
+})
+  .catch(err => console.error(err));
